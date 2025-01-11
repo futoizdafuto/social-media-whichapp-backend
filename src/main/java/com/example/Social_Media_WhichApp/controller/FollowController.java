daf78b2a1e319e229a8cb9befc3d1c1242f7142e
Tingoatweb <21130565@st.hcmuaf.edu.vn>
2025-01-11 19:04:03 +0700

    Update FollowController.java
    Update FollowController.java

--- src/main/java/com/example/Social_Media_WhichApp/controller/FollowController.java
+++ src/main/java/com/example/Social_Media_WhichApp/controller/FollowController.java
@@ -4,6 +4,7 @@ import com.example.Social_Media_WhichApp.services.FollowService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -55,4 +56,27 @@ public class FollowController {
     public List<String> getAllUsernames() {
         return followService.getAllUsernames();
     }
+    @GetMapping("/waitingusers")
+    public Map<String, Object> getWaitingUsers(@RequestParam String username) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            List<String> waitingUsers = followService.getWaitingUsers(username);
+            response.put("status", "success");
+            response.put("waiting_users", waitingUsers);
+        } catch (IllegalArgumentException e) {
+            response.put("status", "error");
+            response.put("message", e.getMessage());
+        }
+        return response;
+    }
+    @PostMapping("/updatestatus")
+    public Map<String, Object> updateFollowStatus(@RequestParam String username,
+                                                  @RequestParam String targetUsername,
+                                                  @RequestParam String token) {
+        return followService.updateFollowStatus(username, targetUsername, token);
+    }
+
+
+
+
 }
