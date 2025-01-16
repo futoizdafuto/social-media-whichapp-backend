d151c03d3b6a9a875b168eb7149e7bf88cf9bca9
hongnhan100 <hongnhan100@gmail.com>
2025-01-17 06:06:43 +0700

    post admin

--- src/main/java/com/example/Social_Media_WhichApp/controller/PostController.java
+++ src/main/java/com/example/Social_Media_WhichApp/controller/PostController.java
@@ -204,7 +204,7 @@ public class PostController {
             }
 
             // Kiểm tra xem có phải chủ post không
-            User currentUser = userService.findUserByUsername(authentication.getName());
+            User currentUser = userService.findUserByUsername(authentication.getNamegit());
             if (!currentUser.getUserId().equals(post.getUser().getUserId()) && (currentUser.getRole().getRole_id() != 1)) {
                 return ResponseEntity.status(HttpStatus.FORBIDDEN)
                         .body("You can only delete your own posts");
